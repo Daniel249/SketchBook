740fc416af3d9e39da600d06b7325e89b5ed5924
Daniel <danielmorillo249@hotmail.com>
2018-11-13 18:50:22 +0100

    commit everything

---
+++ sketch.js
@@ -0,0 +1,97 @@
+function setup() {
+	createCanvas(600, 600);
+	// initialize an ammount of Dots
+	var ammountDots = 10;
+	initDots(ammountDots);
+}
+
+var squareX = 75;
+var squareY = 75;
+var squareSize = 500;
+
+var Dots = []
+
+function draw() {
+
+	background(200);
+
+	// draw Red Square
+	noFill();
+	stroke(255, 0, 0);
+	rect(squareX, squareY, squareSize, squareSize)
+	ellipse(squareX, squareY, 50, 50)
+	// draw Point
+	for (var i = 0; i < Dots.length; i++) {
+		Dots[i].update();
+		Dots[i].checkCollision();
+		Dots[i].drawLines();
+	}
+
+}
+
+
+function Punto(posx, posy) {
+	// position relative to square
+	this.pos_x = squareX + posx;
+	this.pos_y = squareY + posy;
+	// random velocity
+	this.speed_x = posx / 20;
+	this.speed_y = 4 - this.speed_x;
+	// array of lines
+	this.lines_x = [];
+	this.lines_y = [];
+
+	// update position and draw
+	this.update = function() {
+		this.pos_x = this.pos_x + this.speed_x;
+		this.pos_y = this.pos_y + this.speed_y;
+		// set color blue
+		fill(0, 0, 255);
+		// disable outline when drawing
+		noStroke();
+		// draw
+		ellipse(this.pos_x, this.pos_y, 10, 10);
+	}
+
+	// check if collision with square
+	this.checkCollision = function() {
+		if (this.pos_x <= squareX || this.pos_x >= squareX + squareSize) {
+			// reverse horizontal speed
+			this.speed_x *= -1;
+			//save collision point to array
+			this.lines_x.push(this.pos_x);
+			this.lines_y.push(this.pos_y);
+		} else if (this.pos_y <= squareY || this.pos_y >= squareY + squareSize) {
+			this.speed_y *= -1;
+			this.lines_x.push(this.pos_x);
+			this.lines_y.push(this.pos_y);
+		}
+	}
+	// draw lines for each adjacent coordinates in lines_x/y arrays
+	this.drawLines = function() {
+		var last_x = this.lines_x[0];
+		var last_y = this.lines_y[0];
+		// change color to green
+		stroke(0, 100, 100);
+		// loop from second position in array
+		for (var i = 1; i < this.lines_x.length; i++) {
+			// draw line from last saved point to current point
+			line(last_x, last_y, this.lines_x[i], this.lines_y[i]);
+			// update lastSavedPoint = current point
+			last_x = this.lines_x[i];
+			last_y = this.lines_y[i];
+		}
+		// draw from last point in array to current position of Punto
+		line(last_x, last_y, this.pos_x, this.pos_y);
+
+	}
+}
+
+function initDots(num) {
+	for (var i = 0; i < num; i++) {
+		var loc_x = Math.floor(Math.random(0, squareSize))
+		var loc_y = Math.floor(Math.random(0, squareSize))
+		var punto = new Punto(i * 10 + 2, i * 10);
+		Dots.push(punto);
+	}
+}
